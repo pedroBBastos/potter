48739fb22c17b74f59bf71e39cbd23c4cbdd70b3
Pedro Bastos <pedro.bbastos123@gmail.com>
2021-07-16 16:53:13 -0300

    Início challenge

---
+++ POTTER-web/src/main/java/com/challenge/PotterApplication.java
@@ -0,0 +1,13 @@
+package com.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotterApplication.class, args);
+    }
+
+}
