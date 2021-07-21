e454caaa7a8ea921ec67df8545adcc5a9d1796a7
Pedro Bastos <pedro.bbastos123@gmail.com>
2021-07-20 23:16:48 -0300

    Documentação de PersonagemService

--- POTTER-service/src/main/java/com/challenge/personagem/PersonagemService.java
+++ POTTER-service/src/main/java/com/challenge/personagem/PersonagemService.java
@@ -19,6 +19,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Service com implementação das regras de CRUD de personagens.
+ * @author PedroBastos
+ */
+
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonagemService {
@@ -29,11 +34,26 @@ public class PersonagemService {
     private final ValidatorFactory validatorFactory;
 
 
+    /**
+     * Método para criação de novo personagem a partir de personagemCriacaoDTO.
+     *
+     * @param personagemCriacaoDTO - DTO recebido na requisição
+     * @return PersonagemDTO a partir da entidade persistida;
+     */
     public PersonagemDTO criarNovoPersonagem(PersonagemCriacaoDTO personagemCriacaoDTO) {
         var personagemEntity = this.validateCriacaoTO(personagemCriacaoDTO);
         return modelMapper.map(this.personagemRepository.save(personagemEntity), PersonagemDTO.class);
     }
 
+    /**
+     * Método para atualizar personagem a partir de personagemCriacaoDTO.
+     * Antes de atualização ser efetuada, diversas validações de modificação são realizadas.
+     * Aqui é considerado que um personagem pode trocar de casa, já que a
+     * chave da entidade de personagem é o nome.
+     *
+     * @param personagemUpdateDTO - DTO recebido na requisição
+     * @return PersonagemDTO a partir da entidade persistida;
+     */
     public PersonagemDTO atualizarPersonagem(PersonagemUpdateDTO personagemUpdateDTO) {
         PersonagemEntity personagem = this.validateModificacao(personagemUpdateDTO);
         this.validateCasaExistente(personagemUpdateDTO.getHouse());
@@ -42,10 +62,22 @@ public class PersonagemService {
         return modelMapper.map(this.personagemRepository.save(personagem), PersonagemDTO.class);
     }
 
+    /**
+     * Método para deletar personagem a partir de personagemCriacaoDTO.
+     * Antes de atualização ser efetuada, diversas validações de modificação são realizadas.
+     *
+     * @param personagemDTO - DTO recebido na requisição
+     */
     public void deletePersonagem(PersonagemDTO personagemDTO) {
         this.personagemRepository.delete(this.validateModificacao(personagemDTO));
     }
 
+    /**
+     * Método para retornar personagens existentes, podendo ser realizado filtragem pela casa.
+     *
+     * @param house - casa a se filtrar na busca.
+     * @return
+     */
     public List<PersonagemDTO> findAll(String house) {
         return this.findPersonagemEntities(house).stream()
                 .map(personagemEntity ->
@@ -53,12 +85,24 @@ public class PersonagemService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Método para retornar personagem a partir de personagemDTO recebido da requisição.
+     *
+     * @param personagemDTO - DTO recebido na requisição
+     * @return
+     */
     public PersonagemDTO findByPersonagemDTO(PersonagemDTO personagemDTO) {
         this.validateNullTO(personagemDTO);
         this.validateNomeNull(personagemDTO.getName());
         return modelMapper.map(this.personagemRepository.findByName(personagemDTO.getName()), PersonagemDTO.class);
     }
 
+    /**
+     * Método para realizar busca de lista de personagens existentes, podendo ser filtrada por casa.
+     *
+     * @param house - parâmetro para filtragem de personagens por casa. Pode ser nulo.
+     * @return - lista de personagens buscados
+     */
     private List<PersonagemEntity> findPersonagemEntities(String house) {
         if(house == null) {
             return this.personagemRepository.findAll();
@@ -66,6 +110,14 @@ public class PersonagemService {
         return  this.personagemRepository.findAllByHouse(house);
     }
 
+    /**
+     * Método para realizar a atualização de entidade existente em banco a partir
+     * de dados recebidos por DTO da requisição.
+     *
+     * @param personagemEntity - entidade persisitida em banco a se atualizar
+     * @param personagemUpdateDTO - DTO recebida da requisição com valores a se atualizar
+     * @return - entidade atualizada
+     */
     private PersonagemEntity atualizarPersonagemEntityByDTO(PersonagemEntity personagemEntity,
                                                             PersonagemUpdateDTO personagemUpdateDTO) {
         PersonagemEntity atualizado = modelMapper.map(personagemUpdateDTO, PersonagemEntity.class);
@@ -74,6 +126,12 @@ public class PersonagemService {
         return atualizado;
     }
 
+    /**
+     * Método para realizar validação de DTO recebido em requisição para criação de personagem.
+     *
+     * @param personagemCriacaoDTO - DTO recebido na requisição.
+     * @return - entidade validade a ser persistida
+     */
     private PersonagemEntity validateCriacaoTO(PersonagemCriacaoDTO personagemCriacaoDTO) {
         this.validateNullTO(personagemCriacaoDTO);
         this.validatePersonagemExistente(personagemCriacaoDTO);
@@ -85,22 +143,51 @@ public class PersonagemService {
         return personagemEntity;
     }
 
+    /**
+     * Método para validar se personagem a se criar já existe na base de dados.
+     * Verificação realizada a partir da chave única que é o nome do personagem.
+     *
+     * @param personagemCriacaoDTO - DTO recebido em requisição para criação de personagem
+     * @throws PersonagemException caso personagem já exista.
+     */
     private void validatePersonagemExistente(PersonagemCriacaoDTO personagemCriacaoDTO) {
         if(this.personagemRepository.existsByName(personagemCriacaoDTO.getName())) {
             throw new PersonagemException("Personagem já existente!");
         }
     }
 
+    /**
+     * Método que efetua a regra de validação do ID da casa passada na requisição de
+     * criação ou atualização de personagem.
+     *
+     * @param casaId - id da casa passada como parâmetro
+     * @return - CasasDTO existente e respectiva ao id passado
+     * @throws CasaException caso id passado não tenha sido verificado
+     */
     private CasasDTO validateCasaExistente(String casaId) {
         return this.potterAPIHousesClient.getHouses().getHouses().stream()
                 .filter(casasDTO -> casasDTO.getId().equals(casaId))
                 .findFirst().orElseThrow(() -> new CasaException(String.format("Casa '%s' não encontrada!", casaId)));
     }
 
+    /**
+     * Método para efetuar a transformação de um obejto PersonagemCriacaoDTO em um
+     * objeto PersonagemEntity.
+     *
+     * @param personagemCriacaoDTO
+     * @return - PersonagemEntity criado a partir de DTO recebido
+     */
     private PersonagemEntity novoPersonagemByDTO(PersonagemCriacaoDTO personagemCriacaoDTO) {
         return modelMapper.map(personagemCriacaoDTO, PersonagemEntity.class);
     }
 
+    /**
+     * Método para realizar validação de DTO passado em requisição de modificação (atualização ou remoção de personagem).
+     *
+     * @param personagemDTO - DTO passado na requisição
+     * @return - personagem a se modificar, respectivo ao DTO passado na requisição
+     * @throws PersonagemException se personagem não for encontrado
+     */
     private PersonagemEntity validateModificacao(PersonagemDTO personagemDTO) {
         this.validateNullTO(personagemDTO);
         this.validateNomeNull(personagemDTO.getName());
@@ -113,18 +200,36 @@ public class PersonagemService {
         return personagemEntity;
     }
 
+    /**
+     * Método para realizar validação de PersonagemDTO nulo.
+     *
+     * @param personagemDTO
+     * @throws ParametroInvalidoException caso DTO nulo.
+     */
     private void validateNullTO(PersonagemDTO personagemDTO) {
         if(personagemDTO == null) {
             throw new ParametroInvalidoException("Objeto nulo!");
         }
     }
 
+    /**
+     * Método para realizar validação de chave nula (nome nulo).
+     *
+     * @param nome
+     * @throws ParametroInvalidoException caso nome nulo.
+     */
     private void validateNomeNull(String nome) {
         if(nome == null) {
             throw new ParametroInvalidoException("Chave 'name' não informada!");
         }
     }
 
+    /**
+     * Método para realizar validação de colunas de nova entidade a se persistir em banco.
+     *
+     * @param personagemEntity
+     * @throws ParametroInvalidoException caso alguma coluna inválida.
+     */
     private void validateColunasInvalidas(PersonagemEntity personagemEntity) {
         Set<ConstraintViolation<PersonagemEntity>> validate =
                 this.validatorFactory.getValidator().validate(personagemEntity);
